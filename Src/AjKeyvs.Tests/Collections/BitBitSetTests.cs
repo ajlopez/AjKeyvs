115f3dd692cc44e02a52bda7f3a7ae160af25490
ajlopez <webmaster@ajlopez.com>
2011-12-25 20:24:32 -0300

    BigBitSet based on BigArray of bytes, with first tests

---
+++ Src/AjKeyvs.Tests/Collections/BitBitSetTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AjKeyvs.Collections;
+
+namespace AjKeyvs.Tests.Collections
+{
+    [TestClass]
+    public class BitBitSetTests
+    {
+        [TestMethod]
+        public void GetZeroOnEmptySet()
+        {
+            BigBitSet set = new BigBitSet();
+
+            Assert.IsFalse(set[0]);
+            Assert.IsFalse(set[1]);
+            Assert.IsFalse(set[ulong.MaxValue]);
+        }
+
+        [TestMethod]
+        public void SetAndGetFirstOnes()
+        {
+            BigBitSet set = new BigBitSet();
+
+            for (uint k = 0; k < 16; k++)
+                set[k] = true;
+
+            for (uint k = 0; k < 16; k++)
+                Assert.IsTrue(set[k]);
+        }
+
+        [TestMethod]
+        public void SetAndGetLastOnes()
+        {
+            BigBitSet set = new BigBitSet();
+
+            for (uint k = 0; k < 16; k++)
+                set[ulong.MaxValue - k] = true;
+
+            for (uint k = 0; k < 16; k++)
+                Assert.IsTrue(set[ulong.MaxValue - k]);
+        }
+
+        [TestMethod]
+        public void SetAndGetFirstAndLastOnes()
+        {
+            BigBitSet set = new BigBitSet();
+
+            for (uint k = 0; k < 16; k++) 
+                set[ulong.MaxValue - k] = true;
+
+            for (uint k = 0; k < 16; k++)
+                set[k] = true;
+
+            for (uint k = 0; k < 16; k++)
+                Assert.IsTrue(set[ulong.MaxValue - k]);
+
+            for (uint k = 0; k < 16; k++)
+                Assert.IsTrue(set[k]);
+        }
+
+        [TestMethod]
+        public void SetOnesAndGetMiddleZeroes()
+        {
+            BigBitSet set = new BigBitSet();
+
+            for (uint k = 0; k < 16; k++)
+                set[ulong.MaxValue - k] = true;
+            for (uint k = 0; k < 16; k++)
+                set[k] = true;
+
+            for (uint k = 0; k < 256; k++)
+                Assert.IsFalse(set[(ulong) (long.MaxValue - k)]);
+        }
+    }
+}
---
+++ Src/AjKeyvs/Collections/BigBitSet.cs
@@ -0,0 +1,46 @@
+﻿namespace AjKeyvs.Collections
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class BigBitSet
+    {
+        private BigArray<byte> bytes;
+
+        public bool this[ulong index]
+        {
+            get
+            {
+                ulong position = index >> 8;
+                ushort offset = (ushort)(index & 0x07);
+                byte result = this.bytes[position];
+                byte bit = (byte)(1 << offset);
+
+                return (result & bit) != 0;
+            }
+
+            set
+            {
+                if (this.bytes == null)
+                {
+                    if (value == false)
+                        return;
+                    this.bytes = new BigArray<byte>();
+                }
+
+                ulong position = index >> 8;
+                ushort offset = (ushort) (index & 0x07);
+                byte result = this.bytes[position];
+                byte bit = (byte) (1 << offset);
+                if (value == false)
+                    result &= (byte) (0xff - bit);
+                else
+                    result |= bit;
+
+                this.bytes[position] = result;
+            }
+        }
+    }
+}
