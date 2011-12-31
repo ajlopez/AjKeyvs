faea239bde2dce01ba7219bf4004bb5c20049ca6
ajlopez <webmaster@ajlopez.com>
2011-12-31 18:48:16 -0300

    Processor, with set key integer value test

---
+++ Src/AjKeyvs.Server.Tests/ProcessorTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AjKeyvs.Server.Tests
+{
+    [TestClass]
+    public class ProcessorTests
+    {
+        private Repository repository;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.repository = new Repository();
+        }
+
+        [TestMethod]
+        public void ProcessSetKeyValue()
+        {
+            Processor processor = new Processor(this.repository, "set counter 1");
+            processor.Process();
+            Assert.AreEqual(1ul, repository.GetValue("counter"));
+        }
+    }
+}
---
+++ Src/AjKeyvs.Server/Processor.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using AjKeyvs.Server.Parser;
+
+namespace AjKeyvs.Server
+{
+    public class Processor
+    {
+        private Tokenizer tokenizer;
+        private Repository repository;
+
+        public Processor(Repository repository, string text)
+            : this(repository, new StringReader(text))
+        {
+        }
+
+        public Processor(Repository repository, TextReader reader)
+        {
+            this.repository = repository;
+            this.tokenizer = new Tokenizer(reader);
+        }
+
+        public void Process()
+        {
+            string command = this.GetName();
+            string key = this.GetName();
+            ulong value = this.GetIntegerValue();
+            this.GetEndOfCommand();
+
+            if (command == "set")
+                this.repository.SetValue(key, value);
+        }
+
+        private string GetName()
+        {
+            Token token = this.tokenizer.NextToken();
+
+            if (token == null || token.Type != TokenType.Name)
+                throw new InvalidDataException();
+
+            return token.Value;
+        }
+
+        private ulong GetIntegerValue()
+        {
+            Token token = this.tokenizer.NextToken();
+
+            if (token == null || token.Type != TokenType.Integer)
+                throw new InvalidDataException();
+
+            return ulong.Parse(token.Value);
+        }
+
+        private void GetEndOfCommand()
+        {
+            Token token = this.tokenizer.NextToken();
+
+            if (token == null || token.Type == TokenType.EndOfLine)
+                return;
+
+            throw new InvalidDataException();
+        }
+    }
+}
