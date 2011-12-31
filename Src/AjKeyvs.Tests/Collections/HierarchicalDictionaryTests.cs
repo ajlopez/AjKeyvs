37bcc2cf7fb0a5e1d4b42024b420ef0d7827905f
ajlopez <webmaster@ajlopez.com>
2011-12-31 10:27:05 -0300

    HierarchicalDictionary, first tests

---
+++ Src/AjKeyvs.Tests/Collections/HierarchicalDictionaryTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AjKeyvs.Collections;
+
+namespace AjKeyvs.Tests.Collections
+{
+    [TestClass]
+    public class HierarchicalDictionaryTests
+    {
+        private HierarchicalDictionary<string> dictionary;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.dictionary = new HierarchicalDictionary<string>();
+        }
+
+        [TestMethod]
+        public void GetSimpleKeyUndefinedValue()
+        {
+            Assert.IsNull(this.dictionary["foo"]);
+        }
+
+        [TestMethod]
+        public void GetCompositeKeyUndefinedValue()
+        {
+            Assert.IsNull(this.dictionary["users:1"]);
+        }
+    }
+}
---
+++ Src/AjKeyvs/Collections/HierarchicalDictionary.cs
@@ -0,0 +1,22 @@
+﻿namespace AjKeyvs.Collections
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class HierarchicalDictionary<T>
+    {
+        public T this[string key]
+        {
+            get
+            {
+                return default(T);
+            }
+
+            set
+            {
+            }
+        }
+    }
+}
