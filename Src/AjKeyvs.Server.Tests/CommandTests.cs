6430f193902687e6eab40ccc026df73bb44d53ea
ajlopez <webmaster@ajlopez.com>
2012-01-01 17:50:46 -0300

    Command with first test

---
+++ Src/AjKeyvs.Server.Tests/CommandTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AjKeyvs.Server.Tests
+{
+    [TestClass]
+    public class CommandTests
+    {
+        [TestMethod]
+        public void CreateCommandWithoutParameters()
+        {
+            Command command = new Command("get", "users:1:name", null);
+
+            Assert.AreEqual("get", command.Verb);
+            Assert.AreEqual("users:1:name", command.Key);
+            Assert.IsNull(command.Parameters);
+        }
+    }
+}
---
+++ Src/AjKeyvs.Server/Command.cs
@@ -0,0 +1,29 @@
+﻿namespace AjKeyvs.Server
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Command
+    {
+        private string verb;
+        private string key;
+        private IList<object> parameters;
+
+        public Command(string verb, string key, IEnumerable<object> parameters)
+        {
+            this.verb = verb;
+            this.key = key;
+
+            if (parameters != null)
+                this.parameters = new List<object>(parameters);
+        }
+
+        public string Verb { get { return this.verb; } }
+
+        public string Key { get { return this.key; } }
+
+        public IList<object> Parameters { get { return this.parameters; } }
+    }
+}
