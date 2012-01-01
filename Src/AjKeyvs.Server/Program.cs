dbceb937fadb573d6ac8619e63cde2c6535de263
ajlopez <webmaster@ajlopez.com>
2012-01-01 19:49:09 -0300

    AjKeyvs process line command

--- Src/AjKeyvs.Server/Program.cs
+++ Src/AjKeyvs.Server/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using AjKeyvs.Server.Parser;
 
 namespace AjKeyvs.Server
 {
@@ -9,6 +11,33 @@ namespace AjKeyvs.Server
     {
         static void Main(string[] args)
         {
+            Repository repository = new Repository();
+
+            TextReader reader = System.Console.In;
+
+            while (true)
+            {
+                Processor processor = new Processor(repository, new CommandReader(reader, true));
+
+                Console.Write("ajkeyvs> ");
+
+                for (CommandResult result = processor.ProcessCommand(); result != null; result = processor.ProcessCommand())
+                {
+                    if (result.HasValue)
+                    {
+                        if (result.Value is String)
+                        {
+                            Console.Write('"');
+                            Console.Write(result.Value);
+                            Console.WriteLine('"');
+                        }
+                        else
+                            Console.WriteLine(result.Value.ToString());
+                    }
+                    else
+                        Console.WriteLine("OK");
+                }
+            }
         }
     }
 }
