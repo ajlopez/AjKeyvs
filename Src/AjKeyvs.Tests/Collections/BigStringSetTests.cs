9f14878fea25324b78a72c8e6a91e99122dcc64e
ajlopez <webmaster@ajlopez.com>
2012-01-07 17:49:28 -0300

    BigStringSet with first test

---
+++ Src/AjKeyvs.Tests/Collections/BigStringSetTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AjKeyvs.Collections;
+
+namespace AjKeyvs.Tests.Collections
+{
+    [TestClass]
+    public class BigStringSetTests
+    {
+        private BigStringSet set;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.set = new BigStringSet();
+        }
+
+        [TestMethod]
+        public void GetStringOnEmptySet()
+        {
+            Assert.IsFalse(this.set["foo"]);
+        }
+    }
+}
---
+++ Src/AjKeyvs/Collections/BigStringSet.cs
@@ -0,0 +1,23 @@
+﻿namespace AjKeyvs.Collections
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class BigStringSet
+    {
+        public bool this[string index]
+        {
+            get
+            {
+                return false;
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
